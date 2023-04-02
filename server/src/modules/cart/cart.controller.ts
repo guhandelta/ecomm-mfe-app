2a55b1d58d158acaa0dade3d6d788e80ec9cca56
Kala Foods <guhan.delta@gmail.com>
2023-04-01 23:11:06 -0400

    Added additional endpoints to the Cart app to add and remove items from the cart

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,6 +1,9 @@
 import {
   Controller,
   Get,
+  Post,
+  Delete,
+  Body,
   Request,
   UseGuards,
 } from '@nestjs/common';
@@ -41,5 +44,33 @@ export class CartController {
   // The JWT is passed through the req, from which the userId is extarcted
   async index(@Request() req): Promise<Cart> {
     return this.carts[req.user.userId] ?? { cartItems: [] };
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  /* This statement says that the req body has an id of the elementm that the user intends to add, and 
+  return the new-updated cart*/
+  async create(@Request() req, @Body() { id }: {id: string}): Promise<Cart> {
+    const cart = this.carts[req.user.userId];
+    const cartItem = cart.cartItems.find(
+      // Checking if the item already exists in the cart
+      cartItem => cartItem.id === parseInt(id)
+    )
+    if(cartItem){
+      cartItem.quantity += 1;
+    } else{
+      cart.cartItems.push({
+        ...products.find(product => product.id === parseInt(id)),
+        quantity: 1,
+      });
+    };
+    return cart;
+  }
+
+  @Delete()
+  @UseGuards(JwtAuthGuard)
+  async destroy(@Request() req): Promise<Cart>{
+    this.carts[req.user.userId] = { cartItems : [] };
+    return this.carts[req.user.userId]; //Returning the userId of the cart that was emptied
   }
 }
